490c5bf506d0cba86cb0ff9265a4eeb56bfcfc04
Jonathan Deavers <jonathan.deavers@moodle.com>
2025-03-09 17:04:21 -0400

    Init

---
+++ classes/config_observer.php
@@ -0,0 +1,43 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Version information for local_configkeeper plugin.
+ *
+ * @package   local_configkeeper
+ * @copyright 2025 Jon Deavers <jondeavers@gmail.com>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace local_configkeeper;
+
+/**
+ * Observer class for when core config logs are created.
+ */
+class config_observer {
+    /**
+     * Observer.
+     *
+     * @param object $event
+     * @return void
+     */
+    public static function observe_config_log_created(object $event): void {
+        $debug = [
+            'confirm' => 'CONFIG LOG CREATED',
+        ];
+        file_put_contents('/tmp/DEBUG.json', json_encode($debug) . PHP_EOL, FILE_APPEND);
+    }
+}
---
+++ version.php
@@ -0,0 +1,31 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Version information for local_configkeeper plugin.
+ *
+ * @package   local_configkeeper
+ * @copyright 2025 Jon Deavers <jondeavers@gmail.com>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version   = 2025030800;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->requires  = 2022112800;        // Requires Moodle 4.3 (2022112800)
+$plugin->component = 'local_configkeeper'; // Full name of the plugin
+$plugin->maturity  = MATURITY_ALPHA;    // Maturity level
+$plugin->release   = '0.1.0';           // Human-readable version name
